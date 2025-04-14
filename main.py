c0bb5df0f861a8f55bbbc27037a983bf0413ddb5
Fedorov_Maksim <409754@edu.itmo.ru>
2025-04-14 19:49:15 +0300

    tracker

--- main.py
+++ main.py
@@ -7,7 +7,27 @@ from scipy.optimize import approx_fprime, minimize
 #     return approx_fprime(x, f, eps)
 
 
-def gradient(f, x, eps=1e-8):
+
+class Tracker:
+    def __init__(self, g=0, h=0):
+        self._g = g
+        self._h = h
+
+    @property
+    def g(self):
+        return self._g
+    @property
+    def h(self):
+        return self._h
+    @g.setter
+    def g(self, value):
+        self._g = value
+    @h.setter
+    def h(self, value):
+        self._h = value
+
+def gradient(f, x, tracker, eps=1e-8):
+    tracker.g = tracker.g + 1
     grad = np.zeros_like(x, dtype=float)
     for i in range(len(x)):
         x1, x2 = np.array(x, dtype=float), np.array(x, dtype=float)
@@ -20,7 +40,11 @@ def gradient(f, x, eps=1e-8):
 def symbolic_hessian(f_sympy, variables):
     hess = sp.hessian(f_sympy, variables)
     hess_func = sp.lambdify(variables, hess, 'numpy')
-    return lambda x: np.array(hess_func(*x), dtype=float)
+    def wrapped(x, tracker):
+        tracker.h = tracker.h + 1
+        return np.array(hess_func(*x), dtype=float)
+    
+    return wrapped
 
 
 def dichotomy_step(f, x, direction, a=0, b=1, eps=1e-5, c=10):
@@ -41,15 +65,17 @@ def dichotomy_step(f, x, direction, a=0, b=1, eps=1e-5, c=10):
 
 
 def newton_method_dichotomy(f, f_sympy, variables, x0, max_iter=100, eps=1e-6):
+    tracker = Tracker()
     hessian_func = symbolic_hessian(f_sympy, variables)
 
     x = x0.copy()
+    
     for k in range(max_iter):
-        grad = gradient(f, x)
+        grad = gradient(f, x, tracker)
         if np.linalg.norm(grad) < eps:
             break
 
-        H = hessian_func(x)
+        H = hessian_func(x, tracker)
         try:
             direction = np.linalg.solve(H, -grad)
         except np.linalg.LinAlgError:
@@ -59,7 +85,7 @@ def newton_method_dichotomy(f, f_sympy, variables, x0, max_iter=100, eps=1e-6):
         alpha = dichotomy_step(f, x, direction)
         x = x + alpha * direction
 
-    print(f"Newton method with dichotomy: iterations: {k}")
+    print(f"Newton method with dichotomy: iterations: {k}, grad: {tracker.g}, hes: {tracker.h}")
     return x
 
 
@@ -71,8 +97,8 @@ def rosenbrock(x):
     return 100 * (x[1] - x[0] ** 2) ** 2 + (1 - x[0]) ** 2
 
 
-x0 = np.array([-1.5, 1.5])
-# x0 = np.array([-15, 15])
+# x0 = np.array([-gi1.5, 1.5])
+x0 = np.array([-15, 15])
 
 x_min = newton_method_dichotomy(
     f=rosenbrock,
@@ -84,9 +110,10 @@ x_min = newton_method_dichotomy(
 print("minimum:", x_min)
 
 
+tracker = Tracker()
 def scipy_newton_cg(f, x0, eps=1e-6, max_iter=100):
     res = minimize(f, x0, method='Newton-CG',
-                   jac=lambda x: gradient(f, x),
+                   jac=lambda x: gradient(f, x, tracker),
                    options={'xtol': eps})
     print(f"Newton-CG: {res.message}, iterations: {res.nit}, func calc: {res.nfev}, grad: {res.njev}, hes: {res.nhev}")
     return res.x
@@ -94,7 +121,7 @@ def scipy_newton_cg(f, x0, eps=1e-6, max_iter=100):
 
 def scipy_bfgs(f, x0, eps=1e-6, max_iter=100):
     res = minimize(f, x0, method='BFGS',
-                   jac=lambda x: gradient(f, x),
+                   jac=lambda x: gradient(f, x, tracker),
                    options={'gtol': eps})
     print(f"BFGS: {res.message}, iterations: {res.nit}, func calc: {res.nfev}, grad: {res.njev}")
     return res.x
@@ -102,7 +129,7 @@ def scipy_bfgs(f, x0, eps=1e-6, max_iter=100):
 
 def scipy_lbfgs(f, x0, eps=1e-6, max_iter=100):
     res = minimize(f, x0, method='L-BFGS-B',
-                   jac=lambda x: gradient(f, x),
+                   jac=lambda x: gradient(f, x, tracker),
                    options={'ftol': eps})
     print(f"L-BFGS: {res.message}, iterations: {res.nit}, func calc: {res.nfev}, grad: {res.njev}")
     return res.x
